e83b661b161ab1d16738eed593ec5ea4d5cea86c
Galina Kastykina <galina_kastykina@users.noreply.github.com>
2020-04-20 17:21:41 +0300

    state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,28 +6,31 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
   
 class App extends React.Component {
-    render() {
-        const items = [
-            {
-               value: 'Learn React',
+   state = {
+        items: [
+           {
+              value: 'Learn React',
+              isDone: false
+           },
+           {
+               value: 'Read a book', 
                isDone: false
-            },
-            {
-                value: 'Read a book', 
-                isDone: false
-            },
-            {
-                value: 'Watch a movie',
-                isDone: true
-            },
-        ];
-    
+           },
+           {
+               value: 'Watch a movie',
+               isDone: true
+           }
+        ]
+           
+    };
+
+    render() {
         return (
             <div className = {styles.wrap}>
             <h1 className = {styles.header}>todos</h1>
             <div className ={styles.todosWrap}>
             <InputItem />
-            <ItemList items = { items } />
+            <ItemList items = { this.state.items } />
             <Footer count = {3} />
             </div>
         </div>);
