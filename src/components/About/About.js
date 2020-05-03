b5724aa8095415b839d8ef30bf1b8b4fc837badb
Galina Kastykina <galina_kastykina@users.noreply.github.com>
2020-05-03 17:16:12 +0300

    request

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,84 @@
 import React from 'react';
+import styles from './About.module.css';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { Octokit } from '@octokit/rest';
 
-const About = () => (
-    <h1>Обо мне</h1>
-);
+const octokit = new Octokit();
 
-export default About; 
+class About extends React.Component {
+    state = {
+        isLoading: true,
+        repoList: [],
+        userInfo: [],
+        isError: false,
+        errorMessage: ''
+    }
+
+    componentDidMount() {
+        octokit.repos.listForUser({
+            username: 'galigalinochka'
+        }).then(({ data }) =>{ //console.log(data);
+        
+            this.setState({
+                repoList: data,
+                isLoading: false
+            });
+        })
+            .catch(err => {
+            this.setState({
+                isLoading: false,
+                isError: true,
+                errorMessage: err
+            });
+        });
+
+        octokit.users.getByUsername({
+            username: 'nfgnfn'
+          })
+          .then(({ data }) =>{ console.log(data);
+        
+            this.setState({
+                userInfo: data,
+                isLoading: false
+            });
+        })
+            .catch(err => {
+            this.setState({
+                isLoading: false,
+                isError: true,
+                errorMessage: err
+            });
+        });
+
+    }
+
+    render() {
+        const { isLoading, repoList, userInfo, isError, errorMessage } = this.state;
+        return (
+            <div className={styles.wrap}>
+                <h1 className={styles.header1}>Обо мне</h1>
+                {isError ? 'Ошибка. Невозможно отобразить. ' + errorMessage :
+                <div>
+                <div className={styles.info}>
+                <img src ={userInfo.avatar_url} className={styles.avatar} alt='avatar'/>
+                <div>
+                <p className={styles.name}>{userInfo.name ? userInfo.name : userInfo.login}</p>
+                <p className={styles.bio}>{userInfo.bio}</p>
+                </div>
+                </div>
+                <h2 className={styles.header2}>{ isLoading ? <LinearProgress color="secondary" /> : 'Мои репозитории:' }</h2>
+                {!isLoading && <ol className={styles.list}>
+                    {repoList.map(repo => (<li key={repo.id}><a href={repo.html_url} className={styles.link}>
+                        {repo.name}
+                        </a>
+                        </li>))}
+                    </ol>}
+                    </div>
+                }
+            </div>
+        );
+    }
+    
+}
+
+export default About;
