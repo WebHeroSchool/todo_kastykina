c70dc8e8d427bcdbdbfa84f0ffd63a8c338f2741
Galina Kastykina <galina_kastykina@users.noreply.github.com>
2020-06-09 15:45:59 +0300

    some changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,6 +5,9 @@ import { Octokit } from '@octokit/rest';
 import Card from '@material-ui/core/Card';
 import { BrowserRouter as Router} from 'react-router-dom';
 import classnames from 'classnames';
+import ReactPaginate from 'react-paginate';
+import errorUserImg from '../../img/error.png';
+
 
 
 const octokit = new Octokit();
@@ -12,34 +15,67 @@ const octokit = new Octokit();
 class About extends React.Component {
     state = {
         isLoading: true,
-        repoList: [],
+        data: [],
         userInfo: [],
-        isError: false,
-        errorMessage: ''
+        isErrorUser: false,
+        isErrorRepo: false,
+        errorMessage: '',
+        offset: 0,
+        perPage: 5,
+        currentPage: 0
     }
 
-    componentDidMount() {
+    receivedData() {
         octokit.repos.listForUser({
             username: 'galigalinochka'
-        }).then(({ data }) =>{ console.log(data);
+        }).then(({data}) =>{ 
+        
+            
+            
+            const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage);
+            const postData = slice.map(repo => <>
+                <div key={repo.id} className={styles.repoLink}>
+                                    <a href={repo.html_url} className={styles.link}>
+                                        {repo.name}
+                                    </a>
+                                    <div className={styles.aboutRepo}>
+                                        <div className={classnames({
+                                            [styles.aboutRepoLanguage_html]: repo.language === 'HTML',
+                                            [styles.aboutRepoLanguage_css]: repo.language === 'CSS',
+                                            [styles.aboutRepoLanguage_js]: repo.language === 'JavaScript',
+                                            [styles.aboutRepoLanguage_null]: repo.language === null})
+                                        }>
+                                            {repo.language}</div>
+                                        <div className={styles.aboutRepoStar}>{repo.stargazers_count}</div>
+                                        <div className={styles.aboutRepoFork}>{repo.forks_count} </div>
+                                        <div className={styles.aboutRepoUpdated}> Обновлен {new Date(repo.updated_at).toLocaleDateString()} </div>
+                                    </div> </div>
+            </>)
+            
         
             this.setState({
-                repoList: data,
+                pageCount: Math.ceil(data.length / this.state.perPage),
+                   
+                postData,
+                
                 isLoading: false
             });
         })
-            .catch(err => {
+
+        .catch(err => {
             this.setState({
                 isLoading: false,
-                isError: true,
+                isErrorRepo: true,
                 errorMessage: err
             });
         });
+    }
 
+    receivedUserInfo() {
         octokit.users.getByUsername({
-            username: 'galigalinochka'
+            username: 'galigalihka'
           })
-          .then(({ data }) =>{ //console.log(data);
+          .then(({ data }) =>{
         
             this.setState({
                 userInfo: data,
@@ -49,23 +85,46 @@ class About extends React.Component {
             .catch(err => {
             this.setState({
                 isLoading: false,
-                isError: true,
+                isErrorUser: true,
                 errorMessage: err
             });
         });
-
     }
 
+
+    handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
+
+        this.setState({
+            currentPage: selectedPage,
+            offset: offset
+        }, () => {
+            this.receivedData()
+        });
+
+    };
+
+    componentDidMount() {
+        this.receivedData()
+        this.receivedUserInfo() 
+    }
+    
     render() {
-        const { isLoading, repoList, userInfo, isError, errorMessage } = this.state;
-        
+        const { isLoading, userInfo, isErrorUser, isErrorRepo, errorMessage } = this.state;
+             
         return (
             
             <div className={styles.wrap}>
                 { isLoading ? <LinearProgress color="secondary" /> : 
                 <Router>
-                    <Card>
-                        {isError ? 'Ошибка. Невозможно отобразить. ' + errorMessage :
+                    <Card style={{minHeight: '100px'}}>
+                        {isErrorUser ? <div className={styles.errorBlock}>
+                            <img src={errorUserImg} alt='no info' className={styles.error}/>
+                            <p className={styles.errorUserText}>Что-то пошло не так...</p>
+                            <span className={styles.try}>Попробуйте загрузить ещё раз</span>
+
+                        </div>:
                             <div>
                             <div className={styles.info}>
                                 <div className={styles.image}>
@@ -81,33 +140,34 @@ class About extends React.Component {
                             </div>
                         }
                     </Card>
-                   <Card>
-                   <div>
-                     <h2 className={styles.header2}>Мои репозитории:</h2>
-                         <ul className={styles.repoList}>
-                             {repoList.map(repo => (
-                                <div key={repo.id} className={styles.repoLink}>
-                                    <a href={repo.html_url} className={styles.link}>
-                                        {repo.name}
-                                    </a>
-                                    <div className={styles.aboutRepo}>
-                                        <div className={classnames({
-                                            [styles.aboutRepoLanguage_html]: repo.language === 'HTML',
-                                            [styles.aboutRepoLanguage_css]: repo.language === 'CSS',
-                                            [styles.aboutRepoLanguage_js]: repo.language === 'JavaScript',
-                                            [styles.aboutRepoLanguage_null]: repo.language === null})
-                                        }>
-                                            {repo.language}</div>
-                                        <div className={styles.aboutRepoStar}>{repo.stargazers_count}</div>
-                                        <div className={styles.aboutRepoFork}>{repo.forks_count} </div>
-                                        <div className={styles.aboutRepoUpdated}> Обновлен {new Date(repo.updated_at).toLocaleDateString()} </div>
-                                    </div>
-                                </div>))}
-                         </ul>
-                     </div>
-                   </Card>   
-                   </Router>         
+                   <Card style={{minHeight: '100px'}}>
+                    {isErrorRepo ? <div className={styles.error}>{this.state.errorMessage}</div> :
+                    
+                        <div className={styles.repozitories}>
+                            <h2 className={styles.header2}>Мои репозитории:</h2>
+                                {this.state.postData}
+                        </div> 
+                    }
+                      {isErrorRepo ? '' :
+                     <ReactPaginate 
+                    previousLabel={"<"}
+                    nextLabel={">"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={this.state.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={styles.pagination}
+                    subContainerClassName={styles.pagesPagination}
+                    activeClassName={styles.active} /> 
                 }
+                   </Card>  
+                  
+                </Router> 
+                        
+                }
+               
             </div>
             
         );
