5865e8ad81c8edd5546f5f5ccf297debaac573b4
Galina Kastykina <galina_kastykina@users.noreply.github.com>
2020-06-16 23:20:55 +0300

    Updates

---
+++ precache-manifest.64e7f9f875bded45ced176e97a5cd342.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b1ada65dc5569f1ecc02a336942d3b8",
+    "url": "/todo_kastykina/index.html"
+  },
+  {
+    "revision": "99432bbe71687d21c7e8",
+    "url": "/todo_kastykina/static/css/main.81830a87.chunk.css"
+  },
+  {
+    "revision": "4ec72e03770ea198099e",
+    "url": "/todo_kastykina/static/js/2.7842cd48.chunk.js"
+  },
+  {
+    "revision": "06ad0cfa56fa8a2c9ccc7b2fff0ec8e0",
+    "url": "/todo_kastykina/static/js/2.7842cd48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99432bbe71687d21c7e8",
+    "url": "/todo_kastykina/static/js/main.b7905f64.chunk.js"
+  },
+  {
+    "revision": "e00ab70345ba964abf64",
+    "url": "/todo_kastykina/static/js/runtime-main.3bb243e6.js"
+  },
+  {
+    "revision": "7baca73c5a0a861d2643590c9e4de1ff",
+    "url": "/todo_kastykina/static/media/Star.7baca73c.svg"
+  },
+  {
+    "revision": "21ef03cd1ac9172ea79df4f253e21fa2",
+    "url": "/todo_kastykina/static/media/contacts.21ef03cd.png"
+  },
+  {
+    "revision": "0eede15be643e8308984b7feb98aefad",
+    "url": "/todo_kastykina/static/media/error.0eede15b.png"
+  },
+  {
+    "revision": "0087379515fcd3812bb5c85e58cbfe96",
+    "url": "/todo_kastykina/static/media/fork.00873795.svg"
+  },
+  {
+    "revision": "e4f2d6b028d93b8c7988d2132bf13ca2",
+    "url": "/todo_kastykina/static/media/github.e4f2d6b0.svg"
+  },
+  {
+    "revision": "e111984e01abca91521f90fe0cc4ca06",
+    "url": "/todo_kastykina/static/media/instagram.e111984e.svg"
+  },
+  {
+    "revision": "5e0470a9538331ff27bf3fe4bcf49629",
+    "url": "/todo_kastykina/static/media/telegram.5e0470a9.svg"
+  },
+  {
+    "revision": "bdb72f003b910e6d1fefecc8bbb422b2",
+    "url": "/todo_kastykina/static/media/todo_2.bdb72f00.png"
+  },
+  {
+    "revision": "b03024c4d635b0a304e483e1d9fa34d8",
+    "url": "/todo_kastykina/static/media/vk.b03024c4.svg"
+  }
+]);
